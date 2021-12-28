e7e6964bc2c6bdd5b41b3aa62f17b10d8d49a75e
rahulbanik98 <59626411+rahulbanik98@users.noreply.github.com>
2021-12-29 03:46:44 +0530

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,68 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) 
+{
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) 
+{
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length)
+    {
+        slideIndex = 1
+    }
+
+    if (n < 1) 
+    {
+        slideIndex = slides.length
+    }
+    
+    for (i = 0; i < slides.length; i++) 
+    {
+        slides[i].style.display = "none";  
+    }
+
+    for (i = 0; i < dots.length; i++) 
+    {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+function firstslideShow ()
+{
+    document.getElementById("firstImage").src="./slideImg/slide-img-1.jpg";
+}
+
+function secondslideShow ()
+{
+    document.getElementById("firstImage").src="./slideImg/slide-img-2.jpg";
+}
+
+function thirdslideShow ()
+{
+    document.getElementById("firstImage").src="./slideImg/slide-img-3.jpg";
+}
+
+function fourthslideShow ()
+{
+    document.getElementById("firstImage").src="./slideImg/slide-img-4.jpg";
+}
+
+function fifthslideShow ()
+{
+    document.getElementById("firstImage").src="./slideImg/slide-img.jpg";
+}
+
+setInterval(firstslideShow, 2000);
+setInterval(secondslideShow, 4000);
+setInterval(thirdslideShow, 6000);
+setInterval(fourthslideShow, 8000);
+setInterval(fifthslideShow, 100000);
